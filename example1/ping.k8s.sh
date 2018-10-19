9f51aa9aba4fd696b3ce002cdfda9fc813fae0f1
Jorge Salamero Sanz <bencer@cauterized.net>
2018-10-19 14:33:01 +0200

    Fix url

--- example1/ping.k8s.sh
+++ example1/ping.k8s.sh
@@ -1,1 +1,1 @@
-curl -F "s=OK" -F "user=bob" -F "passwd=foobar" -F "ipaddr=localhost" -X POST http://localhost:8000/ping.php
+curl -F "s=OK" -F "user=bob" -F "passwd=foobar" -F "ipaddr=localhost" -X POST http://ping/ping.php
