c259da1d6d1d08c56dd55a8365c6763041814b4f
Jorge Salamero Sanz <bencer@cauterized.net>
2018-10-20 09:50:06 +0200

    Split docker-compose vs Kubernetes scenarios

--- example1/kubernetes/setup_response.sh
+++ example1/kubernetes/setup_response.sh
@@ -1,3 +1,4 @@
+# configure k8s
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
@@ -5,11 +6,14 @@ export KUBECONFIG=$HOME/.kube/config
 kubectl taint nodes --all node-role.kubernetes.io/master-
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 
+# configure helm
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get > get_helm.sh
 chmod 700 get_helm.sh
 ./get_helm.sh
 kubectl create -f https://raw.githubusercontent.com/nestorsalceda/work-environment/master/helm-rbac/rbac-config.yaml
 helm init --service-account tiller
+
+# configure kubeless
 sudo apt install python-pip zip
 wget https://github.com/kubeless/kubeless/releases/download/v1.0.0-alpha.6/kubeless_linux-amd64.zip
 unzip kubeless_linux-amd64.zip
